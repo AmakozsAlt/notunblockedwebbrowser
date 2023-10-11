ffd38027adaf1da1dcddd1db0dc8725e863b797d
AmakozsAlt <147641197+AmakozsAlt@users.noreply.github.com>
2023-10-11 11:14:31 -0500

    Add files via upload

---
+++ link.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+ <html>
+ <head>
+ 	<meta charset="utf-8">
+ 	<meta name="viewport" content="width=device-width, initial-scale=1">
+ 	<title></title>
+ </head>
+ <body>
+ 
+ </body>
+ </i&lt;?php $page=$_POST['text'];
+ header("location: http://$page.com");
+ ?&gt;>
+ </html><body><i>
